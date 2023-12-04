5bed40476b565a15297b647f86e7b878bb03e322
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2023-12-05 00:09:54 +0100

    Make test pass for day 1 - Part 1

--- aoc2023/src/day01/index.js
+++ aoc2023/src/day01/index.js
@@ -1,52 +1,52 @@
 import run from 'aocrunner';
 import { reader } from '../utils/read.js'
 
-const parseInput = (rawInput) => {
-  const inputFile = reader('src/day01/input.txt')
 
-  return inputFile
+const parseInput = (inputFile) => {
+  return inputFile;
 };
 
+
+const extractNumbers = (input) => {
+  let output = []
+  
+  input.map((item) => {
+    let strippedOfLetters = item.replace(/[^0-9]/g, "")
+    output.push(strippedOfLetters)
+  })
+  
+  console.log(output)
+  return output
+} 
+
+const getFirstAndLast = (input) => {
+  let output = []
+  
+  input.map((item) => {
+    let first = item.slice(0,1)
+    let last = item.slice(-1)
+    let firstAndLast = `${first}${last}`
+    
+    output.push(parseInt(firstAndLast))
+  })
+  
+  console.log(output)
+  return output
+}
+
+const addAllNumbers = (input) => {
+  let output = []
+
+  const initialValue = 0
+  output = input.reduce((acc, cur) => acc + cur, initialValue)
+
+  console.log(output)
+  return output
+}
 const part1 = (rawInput) => {
-  
-  const extractNumbers = (input) => {
-    let output = []
-    
-    input.map((item) => {
-      let strippedOfLetters = item.replace(/[^0-9]/g, "")
-      output.push(strippedOfLetters)
-    })
-    
-    console.log(output)
-    return output
-  } 
 
-  const getFirstAndLast = (input) => {
-    let output = []
-    
-    input.map((item) => {
-      let first = item.slice(0,1)
-      let last = item.slice(-1)
-      let firstAndLast = `${first}${last}`
-      
-      output.push(parseInt(firstAndLast))
-    })
-    
-    console.log(output)
-    return output
-  }
-  
-  const addAllNumbers = (input) => {
-    let output = []
-
-    const initialValue = 0
-    output = input.reduce((acc, cur) => acc + cur, initialValue)
-
-    console.log(output)
-    return output
-  }
-
-  const data = parseInput(rawInput)
+  const inputFile = reader('src/day01/input.txt');
+  const data = parseInput(inputFile);
   const extracted = extractNumbers(data)
   const firstAndLast = getFirstAndLast(extracted)
   const finalResult = addAllNumbers(firstAndLast)
@@ -63,10 +63,10 @@ const part2 = (rawInput) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: 'src/day01/input.txt',
+        expected: 54338,
+      },
     ],
     solution: part1,
   },
