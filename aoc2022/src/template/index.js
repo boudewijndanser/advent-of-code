5d88ebacf26547b67ecaceb77014a4a7bb9a78c9
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2024-01-10 11:12:38 +0100

    Add expected variables that change based on testing value + title to template

--- aoc2022/src/template/index.js
+++ aoc2022/src/template/index.js
@@ -6,6 +6,16 @@ const dataSelector = testing
   ? readFile('../aoc2022/src/day01/example.txt')
   : readFile('../aoc2022/src/day01/input.txt')
 
+  const expexted1 = testing 
+  ? 10
+  : 100
+
+  const expexted2 = testing 
+  ? 20
+  : 200
+
+  console.log('--- Title of today ---')
+  console.log('')
 
 const parseInput = (rawData) => {
  const output = rawData
@@ -25,9 +35,6 @@ const part1 = (data) => {
 
 const part2 = (data) => {
   const input = parseInput(dataSelector)
-
-
-  
   const answer2 = ''
   return answer2
 }
@@ -35,19 +42,19 @@ const part2 = (data) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: ``,
+        expected: expexted1,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: ``,
+        expected: expexted2,
+      },
     ],
     solution: part2,
   },
